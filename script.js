6856c5a5c3e17e527355709fca8cc286e2b5ce86
GERSON-NUNES <red.fidelidade@gmail.com>
2024-11-28 08:14:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const logo = document.getElementById('logo');
+
+    logo.addEventListener("click", function() {
+        alert("Logo clicada! Bem-vindo à Rede Fidelidade!");
+    });
+});
